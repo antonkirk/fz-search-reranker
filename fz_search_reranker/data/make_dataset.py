8b091aa8176b09172ae1f1da1fc3777b9fce1b24
s184191 <s184191@student.dtu.dk>
2024-06-14 15:44:51 +0200

    Add draft of data generation with local model

--- fz_search_reranker/data/make_dataset.py
+++ fz_search_reranker/data/make_dataset.py
@@ -4,6 +4,8 @@ from pathlib import Path
 from typing import List
 
 import hydra
+import torch
+import transformers
 from datasets import load_dataset
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
@@ -146,7 +148,37 @@ def make_requests(
 
 
 def generate_queries_with_local_model(model: str, device: str):
-    pass
+    model_id = "aaditya/OpenBioLLM-Llama3-8B"
+
+    pipeline = transformers.pipeline(
+        "text-generation",
+        model=model_id,
+        model_kwargs={"torch_dtype": torch.bfloat16},
+        device="auto",
+    )
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are an expert and experienced from the healthcare and biomedical domain with extensive medical knowledge and practical experience. Your name is OpenBioLLM, and you were developed by Saama AI Labs. who's willing to help answer the user's query with explanation. In your explanation, leverage your deep medical expertise such as relevant anatomical structures, physiological processes, diagnostic criteria, treatment guidelines, or other pertinent medical concepts. Use precise medical terminology while still aiming to make the explanation clear and accessible to a general audience.",
+        },
+        {"role": "user", "content": "How can i split a 3mg or 4mg waefin pill so i can get a 2.5mg pill?"},
+    ]
+
+    prompt = pipeline.tokenizer.apply_chat_template(messages, tokenize=False, add_generation_prompt=True)
+
+    terminators = [pipeline.tokenizer.eos_token_id, pipeline.tokenizer.convert_tokens_to_ids("<|eot_id|>")]
+
+    outputs = pipeline(
+        prompt,
+        max_new_tokens=256,
+        eos_token_id=terminators,
+        do_sample=True,
+        temperature=0.0,
+        top_p=0.9,
+    )
+
+    print(outputs[0]["generated_text"][len(prompt) :])
 
 
 def extract_responses_from_jsonl(chunks, file_path):
@@ -221,7 +253,8 @@ def make_dataset(cfg: DictConfig):
 
 
 if __name__ == "__main__":
-    make_dataset()
+    # make_dataset()
+    generate_queries_with_local_model("aaditya/OpenBioLLM-Llama3-8B-GGUF", "cuda")
 
     # dataset = load_dataset('findzebra/corpus')
     # dataset_train = dataset['train']
